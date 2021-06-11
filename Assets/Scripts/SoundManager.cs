7cf408e26531ff77820d770ab0b100a420f7ab07
hedgehog05 <hedgehog05@users.noreply.github.com>
2021-06-11 07:12:15 +0500

    added music and music mute on the Settings Scene

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioSource musicSource;
+	public static SoundManager instance = null;
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void PlayMusic(AudioClip clip)
+	{
+		musicSource.clip = clip;
+		musicSource.Play();
+	}
+
+	public void StopMusic(AudioClip clip)
+	{
+		musicSource.clip = clip;
+		musicSource.Stop();
+	}
+}
